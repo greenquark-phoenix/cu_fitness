fce95c7d92c4d9ec404ce4057151c32707b16eb7
SudipSarker1 <su_sarke@live.concordia.ca>
2025-02-14 14:59:24 -0500

    all previous work into new main branch

---
+++ meals/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Meal, Ingredient, MealIngredient
+
+class MealIngredientInline(admin.TabularInline):
+    model = MealIngredient
+    extra = 1
+
+class MealAdmin(admin.ModelAdmin):
+    inlines = [MealIngredientInline]
+    list_display = ('meal_name', 'meal_type', 'diet_type', 'cooking_duration', 'cost', 'total_calories')
+
+admin.site.register(Meal, MealAdmin)
+admin.site.register(Ingredient)
