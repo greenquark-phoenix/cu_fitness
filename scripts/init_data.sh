83523fe85098996438b279cbbeee59872f1e2381
mirhamed <mi_mol@live.concordia.ca>
2025-03-16 22:48:19 -0400

    workouts json fixtures file is added

--- scripts/init_data.sh
+++ scripts/init_data.sh
@@ -2,7 +2,7 @@
 
 python manage.py loaddata users
 python manage.py loaddata goals
-python manage.py loaddata workouts
+python manage.py dumpdata workouts --indent 2 --output=workouts/fixtures/workouts_data.json
 python manage.py loaddata meals
 python manage.py loaddata ingredients
 python manage.py loaddata meal_ingredients
