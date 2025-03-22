394019c67949861277c25c921e4f513e261a0272
SudipSarker1 <su_sarke@live.concordia.ca>
2025-03-22 15:18:22 -0400

    Update goals/urls.py back to previous state.

--- goals/urls.py
+++ goals/urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
-from . import views
 
-app_name = "goals"
+from goals import views as goal_views
 
 urlpatterns = [
-    path("", views.goal_list, name="user_goals"),
-    path("create/", views.create_goal, name="create_goal"),
-    path("update/<int:goal_id>/", views.update_goal, name="update_goal"),
-    path("delete/<int:goal_id>/", views.delete_goal, name="delete_goal"),
-    path("log_progress/<int:goal_id>/", views.log_progress, name="log_progress"),
-    path("recommend/", views.recommend_goals, name="recommend_goals"),
-    path("progress_notification/", views.goal_progress_notification, name="goal_progress_notification"),
-    path("create_from_assistant/", views.create_goal_from_assistant, name="create_goal_from_assistant"),
+    path('', goal_views.list_user_goals, name='user_goals')
 ]
