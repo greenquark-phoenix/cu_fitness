91d8200f82b2bb2c15589ed0ccd72edfc93089de
Belal Nwiran <bnwiran@gmail.com>
2025-04-03 20:16:33 -0400

    Fix some issues

--- scripts/init_data.bat
+++ scripts/init_data.bat
@@ -4,5 +4,3 @@ python manage.py loaddata users
 python manage.py loaddata goals
 python manage.py loaddata workouts
 python manage.py loaddata meals
-python manage.py loaddata ingredients
-python manage.py loaddata meal_ingredients
