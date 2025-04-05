720da054bc161fe9f434d90a750fe13d8db0ebad
Belal Nwiran <bnwiran@gmail.com>
2025-04-04 22:48:50 -0400

    cleanup

--- scripts/init_data.sh
+++ scripts/init_data.sh
@@ -4,3 +4,5 @@ python manage.py loaddata users
 python manage.py loaddata goals
 python manage.py loaddata workouts
 python manage.py loaddata meals
+python manage.py loaddata schedule
+python manage.py loaddata mylist
